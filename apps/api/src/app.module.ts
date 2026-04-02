9a8d0a33502f6855665f3b2694ec243ececb7112
Bryan Pérez <bpballadares57@gmail.com>
2026-04-02 11:17:48 -0600

    feat: Add Prisma module and connect to Supabase

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { QuestionsModule } from './modules/questions/question.module';
 import { InterviewsModule } from './modules/interviews/interviews.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [QuestionsModule, InterviewsModule],
+  imports: [PrismaModule, QuestionsModule, InterviewsModule],
 })
 export class AppModule {}
